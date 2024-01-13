3569d4794a1df383d3754736cb5ebc153c5ccf91
ljy666613 <1042935581@qq.com>
2024-01-13 12:19:44 +0800

    Element Plus

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,7 @@
 // const request = axios.create({
 //     //baseUrl:'/api'
 //     timeout: 5000,
-//     // headers: { "Authorization": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzA1NDgxNjEsImlhdCI6MTY2NDU0ODIyMSwidWlkIjoxMn0.V32v_BUknBmeYDrX5xe6zIszXz_V5UDtLwW_rbBouU5xVY8jh7woES_8CfuyhttgxG5vl8ulHCnuM0DYJKXOT_FPEzQr-Bgh_zFrbE5YuwQFtVugMX3l43ZFNqNgtqSESM2gOyMPi0AwbIBUXbWTdIfjsLvimRQpk_N3xDKRRjiKHuBYzwM0BnxvZcSe0zZsrRdvUsfAUPW8jdyMvcYg8pZLUy3YU1Ff_fsiLVVTYQswskuTZEzAuJ0qNY_vVMSNXbGhkaxCGbx4G-3kLvhpaV5kOua1m7hGXV2BVE1aBkOWqPbcd5hWE3kI6u0qu7FEHL9SW5y1ssAuCYbsFl8W0g" }
+//     // headers: { "Authorization": "" }
 //
 // })
 //
@@ -14,7 +14,7 @@
 // request.interceptors.request.use(config => {
 //     config.headers['Content-Type'] = 'application/json;charset=utf-8';
 //
-//     config.headers['Authorization'] = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzA1NDgxNjEsImlhdCI6MTY2NDU0ODIyMSwidWlkIjoxMn0.V32v_BUknBmeYDrX5xe6zIszXz_V5UDtLwW_rbBouU5xVY8jh7woES_8CfuyhttgxG5vl8ulHCnuM0DYJKXOT_FPEzQr-Bgh_zFrbE5YuwQFtVugMX3l43ZFNqNgtqSESM2gOyMPi0AwbIBUXbWTdIfjsLvimRQpk_N3xDKRRjiKHuBYzwM0BnxvZcSe0zZsrRdvUsfAUPW8jdyMvcYg8pZLUy3YU1Ff_fsiLVVTYQswskuTZEzAuJ0qNY_vVMSNXbGhkaxCGbx4G-3kLvhpaV5kOua1m7hGXV2BVE1aBkOWqPbcd5hWE3kI6u0qu7FEHL9SW5y1ssAuCYbsFl8W0g";  // 设置请求头
+//     config.headers['Authorization'] = "";  // 设置请求头
 //     // config.headers['token'] = user.token;  // 设置请求头
 //     //取出sessionStorage里面的用户信息
 //     let userJson = sessionStorage.getItem("user");
